7da7b54649351eea4711ffa0d50afb75c4dd2465
Ana Helena <anahelenarp@hotmail.com>
2026-01-11 13:54:47 -0300

    refactor: set toast autoclose delay

--- src/utils/toast.ts
+++ src/utils/toast.ts
@@ -1,26 +1,24 @@
 import { toast as reactToastify, ToastOptions } from 'react-toastify';
 
-// Default toast options
 const defaultOptions: ToastOptions = {
   position: 'top-right',
-  autoClose: 4000,
+  autoClose: 3000,
   hideProgressBar: false,
   closeOnClick: true,
   pauseOnHover: true,
   draggable: true,
 };
 
-// Toast utility wrapper
 export const toast = {
   success: (message: string, options?: ToastOptions) => {
     reactToastify.success(message, { ...defaultOptions, ...options });
   },
 
   error: (message: string, options?: ToastOptions) => {
-    reactToastify.error(message, { 
-      ...defaultOptions, 
+    reactToastify.error(message, {
+      ...defaultOptions,
       autoClose: 5000, // Longer for errors
-      ...options 
+      ...options
     });
   },
 
@@ -32,4 +30,3 @@ export const toast = {
     reactToastify.warning(message, { ...defaultOptions, ...options });
   },
 };
-
