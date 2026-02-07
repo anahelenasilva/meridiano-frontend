64ea76c2501c72659859dae5566ef4a139741335
Ana Helena <anahelenarp@hotmail.com>
2026-02-07 16:24:06 -0300

    fix: docker deploy

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  ...(process.env.BUILD_STANDALONE === 'true' && { output: 'standalone' }),
   images: {
     remotePatterns: [
       {
